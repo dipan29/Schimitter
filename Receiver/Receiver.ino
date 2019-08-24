f2f3a1ff3ac7a517404c951a9bdf0aa3e65c3a6c
Dipan Roy <dipan29@users.noreply.github.com>
2019-08-24 14:02:33 +0530

    v 0.9
    Motor Driver Code

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -9,6 +9,16 @@
 #define CLIENT_ADDRESS 5   
 #define SERVER_ADDRESS 9
 
+// Motor A Connections
+int enA = 3;
+int in1 = 2;
+int in2 = 4;
+
+// Motor B Connections
+int enB = 5;
+int in3 = 6;
+int in4 = 7;
+
 // Create an instance of the radio driver
 RH_NRF24 RadioDriver;
 
@@ -26,6 +36,14 @@ void setup()
   // Setup Serial Monitor
   Serial.begin(9600);
   
+  // Set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  
   // Initialize RadioManager with defaults - 2.402 GHz (channel 2), 2Mbps, 0dBm
   if (!RadioManager.init())
     Serial.println("init failed");
@@ -39,22 +57,43 @@ void loop()
     uint8_t len = sizeof(buf);
     uint8_t from;
     if (RadioManager.recvfromAck(buf, &len, &from))
- //Serial Print the values of joystick
-    {
-      Serial.print("got request from : 0x");
-      Serial.print(from, HEX);
-      Serial.print(": X = ");
-      Serial.print(buf[0]);
-      Serial.print(" Y = ");
-      Serial.print(buf[1]);
-      Serial.print(" Z = ");
-      Serial.println(buf[2]);
 
+    {
+
+      //Serial Print the values of joystick
+      //Serial.print("got request from : 0x");
+      //Serial.print(from, HEX);
+      //Serial.print(": MotorA = ");
+      //Serial.print(buf[0]);
+      //Serial.print(" MotorB = ");
+      //Serial.print(buf[1]);
+      //Serial.print(" Dir = ");
+      //Serial.println(buf[2]);
+      
+      // Set Motor Direction
+      if (buf[2] == 1)
+      {
+    // Motors are backwards
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    }else{
+    // Motors are forwards
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+     }
+ 
+      
+      // Drive Motors
+      analogWrite(enA, buf[1]);
+      analogWrite(enB, buf[0]);
+     
       // Send a reply back to the originator client, check for error
       if (!RadioManager.sendtoWait(ReturnMessage, sizeof(ReturnMessage), from))
         Serial.println("sendtoWait failed");
     }
-  }  
-
-  delay(50);
+  }              
 }
