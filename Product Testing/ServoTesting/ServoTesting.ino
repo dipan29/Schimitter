9d96942e33ed958122dd35ad42d03e58064a12d1
Dipan Roy <38533808+dipan29@users.noreply.github.com>
2019-10-20 14:52:21 +0530

    Servo Testing Method

--- Product Testing/ServoTesting/ServoTesting.ino
+++ Product Testing/ServoTesting/ServoTesting.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ServoTimer2.h>
 
+ServoTimer2 servo;
+int value = 0;
+int c_value = 750;
+
+void setup() {
+  Serial.begin(9600);
+  servo.attach(9); // Attach Servo to Pin 9
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+  Serial.println("Enter a Degree Value : - ");
+  while(value = Serial.read()) {
+    if(value > 180 || value < 0) {
+      Serial.println("Error! Enter value withing 0 - 180 Range");
+      break;
+    } else {
+      c_value = map(value,0,180,750,2250);
+      Serial.print("Sending ");
+      Serial.print(value);
+      Serial.print(" Degrees | ");
+      Serial.print(c_value);
+      Serial.println(" to the servo.\n");
+      servo.write(c_value);
+    }
+  }
 }
